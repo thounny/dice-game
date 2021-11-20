f303f2e62fa5569c486fbf4af385f89fc2673c27
thounny <thounnykeo@gmail.com>
2021-11-19 22:02:33 -0500

    var random for dice game and created if/else if statements

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-var randomNumber1 = Math.random() * 7;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // random number between 1 and 6
+
+var randomDiceImg = "dice" + randomNumber1 + ".png"; // string from dice1.png to dice6.png = change source of img to random number
+
+var randomImgSource = "images/" + randomDiceImg; // Images/dice1.png to Images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImgSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImgSource2 = "images/dice" + randomNumber2 + ".png"; // string from and change source of img to random Images/dice1.png to Images/dice6.png
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImgSource2);
+
+
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "💦💥😎😈💯✔💲™ Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "💦💥😎😈💯✔💲™ Player 2 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "DRAW! 💤💦💤💦";
+
+}
