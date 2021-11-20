7ab95c4848da229c6a89ebbc7c3e0775e91f0161
thounny <thounnykeo@gmail.com>
2021-11-19 22:10:14 -0500

    Refactored code for randomNumber1, randomImgSource1

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; // random number between 1 and 6
 
-var randomDiceImg = "dice" + randomNumber1 + ".png"; // string from dice1.png to dice6.png = change source of img to random number
+var randomImgSource1 = "images/dice" + randomNumber1 + ".png"; // string change soure of Images/dice1.png to Images/dice6.png
 
-var randomImgSource = "images/" + randomDiceImg; // Images/dice1.png to Images/dice6.png
+document.querySelectorAll("img")[0].setAttribute("src", randomImgSource1);
 
-var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImgSource);
-
+// dice 2 random number generator
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
